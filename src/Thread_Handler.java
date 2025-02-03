1f4a25ac0977929af2efe7ae1f44d9e21de2235b
Jamy <jamy.d.vries@gmail.com>
2025-02-03 18:40:05 -0500

    Add files via upload

---
+++ src/Thread_Handler.java
@@ -0,0 +1,12 @@
+public class Thread_Handler {
+
+//     Open socket()
+// Send command
+// Collect result
+// Time result
+// Calculate per thread time
+
+    public static void main(String[] args) {
+        
+    }
+}

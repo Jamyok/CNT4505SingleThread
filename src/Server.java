72a516c7f1c0d5b714d98f00d3d59c0960dd1e00
Jamy <jamy.d.vries@gmail.com>
2025-02-08 15:50:17 -0500

    Added basic server communications.
    On branch Basic_Comms
    Changes to be committed:
    	modified:   bin/Client.class
    	modified:   bin/Server.class
    	modified:   bin/Thread_Handler.class
    	new file:   portConfig.txt
    	modified:   src/Client.java
    	modified:   src/Server.java
    
    Changes not staged for commit:
    	modified:   bin/Client.class
    	modified:   bin/Server.class

--- src/Server.java
+++ src/Server.java
@@ -1,9 +1,13 @@
-public class Server {
+import java.net.*;
+import java.io.*;
+import java.util.Scanner;
 
+public class Server {
+//     Server-TO-DO
 //     Includes
 
-// Query user for port to listen on (port)
-// Create server socket(port)
+// Query user for port to listen on (port) (DONE)
+// Create server socket(port) (DONE)
 
 // while(true){
 // Accept user command(socket)
@@ -13,7 +17,40 @@ public class Server {
 // Send back the result to the client(socket)
 // }
 
-    public static void main(String[] args) {
-        
-    }
-}
+public static void main(String[] args) {
+    try {
+        // Read port from portConfig.txt
+        int portAdress;
+        try (Scanner fileScanner = new Scanner(new File("portConfig.txt"))) {
+            portAdress = fileScanner.nextInt();
+        }//end try scanner
+
+        try (// Create Server Socket
+        ServerSocket ss = new ServerSocket(portAdress)) {
+            System.out.println("Server started on port " + portAdress + ", waiting for client...");
+
+            Socket s = ss.accept();
+
+            //receive msg FROM client
+            InputStreamReader in = new InputStreamReader(s.getInputStream());
+            BufferedReader bf = new BufferedReader(in);
+
+            String str = bf.readLine();
+            System.out.println("Client"+ str);
+
+            //Messege sent TO client
+            PrintWriter pr = new PrintWriter(s.getOutputStream());
+            pr.println("Hello Client!");
+            pr.flush();
+
+
+        }//end try serversocket
+        System.out.println("Client connected!");
+    }//end try
+
+    catch (Exception e) {
+        System.out.println("Exception in Server: " + e.getMessage());
+    }//end catch
+
+}//end main
+}//end server class
